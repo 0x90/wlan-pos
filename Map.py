9bbf7c1e1050b83076d4648f3d620f0b6b2c7546
YanXT_X200 <YanXT_X200@localhost>
2010-03-08 18:06:08 +0800

    Add GMap wrapper call in test module. Add Point class to Map module.

--- Map.py
+++ Map.py
@@ -1,15 +1,8 @@
 #!/usr/bin/env python
-"""
-GMap: Holds all the maps and necesary html/javascript for a complete page/view. This allows you to hold more than one map per page.
-Map: A map object contains map properties and a list of points.
-Icon: Icon properites that can be used by your points.
 
-A short howto:
-http://lycos.dropcode.net/gregarius/Lonely_Code/2008/12/04/Google_Maps_and_Django
-"""
 
 class Icon:
-    '''Get/make marker icons at http://mapki.com/index.php?title=Icon_Image_Sets'''
+    """ Icon properities that can be used by different icon type of Point """
     def __init__(self, id='icon'):
         self.id = id
         self.image = ""             # default Google Maps icon
@@ -19,14 +12,25 @@ class Icon:
         self.iconAnchor = (6, 20)
         self.infoWindowAnchor = (5, 1)
 
+
+class Point:
+    """ icon properities that can be used by your points """
+    def __init__(self, loc=[39.92263,116.47287], txt='new point', iconid='icon'):
+        self.loc = loc       # (lat, lon)
+        self.txt = txt       # comment
+        self.iconid = iconid # id of icon type
+
+    def getAttrs(self):
+        return [ self.loc[0], self.loc[1], self.txt, self.iconid ]
+
         
 class Map:
-    """Basic Map class"""
+    """ Basic map class that contains map properties and a list of points """
     def __init__(self, id="map", pointlist=None):
         self.id      = id      # div id        
-        self.width   = "500px" # map div width
-        self.height  = "300px" # map div height
-        self.center  = (39.9226251856,116.472770962)  # center of first view
+        self.width   = "1000px" # map div width
+        self.height  = "600px" # map div height
+        self.center  = (39.9226251856,116.472770962)  # center of init view
         self.zoom    = "1"     # zoom level
         self.navctls = True    # show google map navigation controls
         self.mapctls = True    # show toogle map type (sat/map/hybrid) controls
@@ -36,13 +40,16 @@ class Map:
     def __str__(self):
         return self.id
     
-    def setpoint(self, point):
+    def addpoint(self, point):
         """ Add a point (lat, long, html, icon) """
         self.points.append(point)
+
 
 class GMap:
     """
     Python wrapper class for Google Maps API.
+    GMap Holds all the maps and necesary html/javascript for a complete page/view. 
+    It allows you to hold more than one map per page.
     """
     
     def __str__(self):
@@ -50,14 +57,27 @@ class GMap:
     
     def __init__(self, key=None, maplist=None, iconlist=None):
         """ Default values """
-        if key == None:
-            self.key = "ABQIAAAAQQRAsOk3uqvy3Hwwo4CclBTrVPfEE8Ms0qPwyRfPn-\
-                    DOTlpaLBTvTHRCdf2V6KbzW7PZFYLT8wFD0A"      # google key
+        if key == None:  # google key
+            #self.key = "ABQIAAAAQQRAsOk3uqvy3Hwwo4CclBTrVPfEE8Ms0qPwyRfPn-\
+            #        DOTlpaLBTvTHRCdf2V6KbzW7PZFYLT8wFD0A"     # borrowed 
+            self.key = "ABQIAAAAUSoeJtepJhkFJBWO1OU4thR2L4ipJp-\
+                    hfOESjcOm0U8jmW_-4RSJ9nbM2O84oU28-VALYyIZBa0JmQ" # own
         else: self.key = key
         if maplist == None: self.maps = [ Map() ]
         else: self.maps = maplist
         if iconlist == None: self.icons = [ Icon() ]
         else: self.icons = iconlist
+
+        self.chkIcons()
+
+    def chkIcons(self):
+        """ Icon id validation state check """
+        map_icons = [ icon.id for icon in self.icons ]
+        for map in self.maps:
+            for point in map.points:
+                if not point.iconid in map_icons:
+                    print 'WARN: invalid iconid: \'%s\'! Correnting it...' % point.iconid
+                    point.iconid = 'icon'       # default icon id
     
     def addicon(self, icon):
         self.icons.append(icon)
@@ -67,9 +87,7 @@ class GMap:
         if map.navctls:
             return  "%s%s.gmap.addControl(new GSmallMapControl());\n" % \
                 ('\t'*4, map.id)
-        else:
-            return ""    
-    
+        else: return ""    
     
     def _mapcontroljs(self,map):
         """ Returns the javascript for google maps control"""    
@@ -77,12 +95,10 @@ class GMap:
             return  "%s%s.gmap.addControl(new GMapTypeControl());\n\n\
                 %s.gmap.setMapType(G_SATELLITE_MAP);\n" % \
                 ('\t'*4, map.id, map.id)
-        else:
-            return ""     
-    
+        else: return ""     
     
     def _mapjs(self,map):
-        js = "%s_points = %s;\n" % (map.id, map.points)
+        js = "%s_points = %s;\n" % (map.id, [point.getAttrs() for point in map.points])
         
         js = js.replace("(", "[")
         js = js.replace(")", "]")
@@ -94,7 +110,6 @@ class GMap:
               ('\t'*4, map.id, map.id, map.id, map.center[0], map.center[1], map.zoom, 
                self._mapcontroljs(map), self._navcontroljs(map))
         return js
-    
     
     def _iconjs(self,icon):
         js = """var %s = new GIcon(); 
@@ -114,18 +129,21 @@ class GMap:
     def _buildicons(self):
         js = ""
         if (len(self.icons) > 0):
-            for icon in self.icons: js = js + self._iconjs(icon)    
+            for icon in self.icons: 
+                js = js + self._iconjs(icon) + '\n\t\t\t\t\t'  
         return js
     
     def _buildmaps(self):
         js = ""
-        for map in self.maps: js = js + self._mapjs(map) + '\n'
+        for map in self.maps: 
+            js = js + self._mapjs(map) + '\n'
         return js
 
     def gmapjs(self):
         """ Returns complete javacript for rendering google map """
         
-        self.js = """\n<script src=\"http://maps.google.com/maps?file=api&amp;v=2&amp;key=%s\" type="text/javascript"></script>
+        self.js = """\n<script src=\"http://maps.google.com/maps?file=api&amp;v=2&amp;\
+                sensor=false&amp;key=%s\" type="text/javascript"></script>
         <script type="text/javascript">
 
         function load() {
@@ -156,13 +174,15 @@ class GMap:
                   
                   function array2points(map_points) {            
                       for (var i in map_points) {  
-                        points[i] = new Point(map_points[i][0],map_points[i][1],map_points[i][2],map_points[i][3]);         }
+                        points[i] = new Point(map_points[i][0], map_points[i][1], \
+                                map_points[i][2], map_points[i][3]);         }
                       return points;   
                     }                  
                   
                   function addmarker(point) {
                      if (point.html) {
-                       GEvent.addListener(point.gpoint, "click", function() { // change click to mouseover or other mouse action
+                       // change click to mouseover or other mouse action
+                       GEvent.addListener(point.gpoint, "click", function() { 
                            point.gpoint.openInfoWindowHtml(point.html);
                         
                        });
@@ -174,6 +194,7 @@ class GMap:
                   this.markerlist(this.points);
             }  
                     %s
+
                     %s
             }
         }
@@ -186,23 +207,25 @@ class GMap:
     
     
         
-    def genHtml(self):
+    def genHTML(self):
         """returns a complete html page with google map(s)"""
-        
+        # select the max value for width/height of all self.maps
+        map_width  = max([ map.width  for map in self.maps ])
+        map_height = min([ map.height for map in self.maps ])
         self.html = """
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-  <head>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
-    <title>GMap v2</title>
-    %s
-  </head>
+            <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+            "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+            <html xmlns="http://www.w3.org/1999/xhtml">
+              <head>
+                <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+                <title>GMap v2</title>
+                %s
+              </head>
 
-  <body onload="load()" onunload="GUnload()">
-    <div id="map" style="width: 1000px; height: 600px"></div>
-  </body>
-</html> """ % (self.gmapjs())
+              <body onload="load()" onunload="GUnload()">
+                <div id="map" style="width: %s; height: %s"></div>
+              </body>
+            </html> """ % (self.gmapjs(), map_width, map_height)
         return self.html
 
 
@@ -219,9 +242,11 @@ if __name__ == "__main__":
         print 'id:\'%-5s\' img:\'%s\'' % (icon.id, icon.image)
 
     gmap.maps[0].zoom = 17
-    point = [39.9226251856, 116.472770962, 'hello,<u>world</u>', 'icon2']     
-    gmap.maps[0].setpoint(point)
+    apoint = Point(loc=[39.922625,116.472771], txt='<u>hello</u>!', iconid='icon2')     
+    gmap.maps[0].addpoint(apoint)
     print 'maps: \n%s' % ('-'*35)
-    for map in gmap.maps: print 'id:\'%-5s\'pts:\n\'%s\'' % (map.id, map.points)
+    for map in gmap.maps: 
+        for point in map.points:
+            print 'id:\'%-5s\'pts:\n\'%s\'' % (map.id, point.getAttrs())
     
-    open('map.htm','wb').write(gmap.genHtml())   # generate test file
+    open('map.htm','wb').write(gmap.genHTML())   # generate test file
--- test.py
+++ test.py
@@ -8,6 +8,7 @@ from online import fixPos, getWLAN
 from GPS import getGPS
 from config import WLAN_FAKE, DATPATH, LOCSUFFIX, RADIUS
 from GEO import dist_on_unitshpere
+from Map import GMap, Icon, Map, Point
 
 
 def usage():
@@ -65,6 +66,7 @@ def main():
             usage(); sys.exit(0)
         elif o in ("-v", "--verbose"):
             verbose = True
+            pp = PrettyPrinter(indent=2)
         else:
             print 'Parameter NOT supported: %s' % o
             usage(); sys.exit(99)
@@ -80,36 +82,56 @@ def main():
             sys.exit(99)
 
     # Get WLAN scanning results.
-    len_visAPs, wifis = getWLAN(wlanfake)
+    #len_visAPs, wifis = getWLAN(wlanfake)
 
     # Fix current position.
-    fixloc = fixPos(len_visAPs, wifis, verbose)
-    #fixloc = [ 39.922848,116.472895 ]
+    #fixloc = fixPos(len_visAPs, wifis, verbose)
+    fixloc = [ 39.922848,116.472895 ]
     print 'fixed location: \n%s' % fixloc
 
     # Get GPS referenced Position.
-    refloc = getGPS()
-    #refloc = [ 39.922948,116.472895 ]
+    #refloc = getGPS()
+    refloc = [ 39.922648,116.472895 ]
     print 'referenced location: \n%s' % refloc
 
     # Log the fixed and referenced positioning record.
     # Logging format: [ timestamp, MAC1|MAC2..., fLat, fLon, rLat, rLon, error(meter) ].
     timestamp = time.strftime('%Y-%m%d-%H%M')
-    visMACs = '|'.join(wifis[0])
-    error = dist_on_unitshpere(fixloc[0], fixloc[1], refloc[0], refloc[1])*RADIUS
-    locline = [ timestamp, visMACs, fixloc[0], fixloc[1], refloc[0], refloc[1], error ]
-    print locline
+    #visMACs = '|'.join(wifis[0])
+    #error = dist_on_unitshpere(fixloc[0], fixloc[1], refloc[0], refloc[1])*RADIUS
+    #locline = [ timestamp, visMACs, fixloc[0], fixloc[1], refloc[0], refloc[1], error ]
+    #print 'locline:\n%s' % locline
 
-    date = time.strftime('%Y-%m%d')
-    locfilename = DATPATH + date + LOCSUFFIX
-    dumpCSV(locfilename, locline)
+    #date = time.strftime('%Y-%m%d')
+    #locfilename = DATPATH + date + LOCSUFFIX
+    #dumpCSV(locfilename, locline)
 
+    icon_fixloc = Icon('fixloc'); icon_refloc = Icon('refloc')
+    icon_fixloc.image  = "kml/icons/reddot.png"
+    icon_refloc.image  = "kml/icons/yellowdot.png"
+    icon_fixloc.shadow = icon_refloc.shadow = "kml/icons/dotshadow.png"
+
+    pFixloc = Point(loc=fixloc, txt='alg: ' + '<br>' + str(fixloc), iconid='fixloc')     
+    pRefloc = Point(loc=refloc, txt='gps: ' + '<br>' + str(refloc), iconid='refloc')     
+
+    gmap = GMap(maplist=[Map(pointlist=[pFixloc, pRefloc])], iconlist=[icon_fixloc, icon_refloc])
+    gmap.maps[0].width  = "1260px"; gmap.maps[0].height = "860px"
+    gmap.maps[0].zoom   = 17
+
+    print '\nicon types: (img: null when default)\n%s' % ('-'*35)
+    for icon in gmap.icons: print 'id:\'%-5s\' img:\'%s\'' % (icon.id, icon.image)
+    print '\nmaps: \n%s' % ('-'*35)
+    for map in gmap.maps: 
+        print 'id:\'%s\',\tpoints:' % map.id
+        for point in map.points: print point.getAttrs()
+
+    open('map.htm', 'wb').write(gmap.genHTML())   # generate test file
 
 
 if __name__ == "__main__":
     try:
         import psyco
-        #psyco.bind(scanWLAN_OS)
+        psyco.bind(main)
         #psyco.full()
         #psyco.log()
         #psyco.profile(0.3)
