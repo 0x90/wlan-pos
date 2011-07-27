11777013c00f7c253d8ce43f0f805408107dc83e
YanXT_X200 <YanXT_X200@localhost>
2011-07-27 09:45:37 +0800

    Add PosReq/PosRes msg content support for Area location. Mod uilt.geolocation_api.connect_retry().

---
+++ wpp/util/geolocation_api.py
@@ -0,0 +1,316 @@
+#!/usr/bin/env python
+import sys
+import os
+import csv
+import time
+import urllib2 as ul
+import socket as sckt
+import numpy as np
+import pprint as pp
+import simplejson as json
+import sqlite3 as db
+#import logging
+#wpplog = logging.getLogger('wpp')
+from wpp.config import termtxtcolors as colors
+ 
+
+def genLocReq(macs=None, rsss=None, cellinfo={}, atoken=None):
+    """
+    Request format:
+    {
+      "version": "1.1.0",
+      "host": "maps.google.com",
+      "access_token": "2:k7j3G6LaL6u_lafw:4iXOeOpTh1glSXe",
+      "home_mobile_country_code": 310,
+      "home_mobile_network_code": 410,
+      "radio_type": "gsm",
+      "carrier": "Vodafone",
+      "request_address": true,
+      "address_language": "en_GB",
+      "location": {
+        "latitude": 51.0,
+        "longitude": -0.1
+      },
+      "cell_towers": [
+        {
+          "cell_id": 42,
+          "location_area_code": 415,
+          "mobile_country_code": 310,
+          "mobile_network_code": 410,
+          "age": 0,
+          "signal_strength": -60,
+          "timing_advance": 5555
+        }, ],
+      "wifi_towers": [
+        {
+          "mac_address": "01-23-45-67-89-ab",
+          "signal_strength": 8,
+          "age": 0
+        },
+      ]
+    }
+    Response format:
+    {
+      "location": {
+        "latitude": 51.0,
+        "longitude": -0.1,
+        "altitude": 30.1,
+        "accuracy": 1200.4,
+        "altitude_accuracy": 10.6,
+        "address": {
+          "street_number": "100",
+          "street": "Amphibian Walkway",
+          "postal_code": "94043",
+          "city": "Mountain View",
+          "county": "Mountain View County",
+          "region": "California",
+          "country": "United States of America",
+          "country_code": "US"
+        }
+      },
+      "access_token": "2:k7j3G6LaL6u_lafw:4iXOeOpTh1glSXe"
+    }
+    """
+    req = {}; cellinfo_keys = cellinfo.keys()
+    if 'lac' in cellinfo_keys and 'cid' in cellinfo_keys:
+        lac = cellinfo['lac']; cid = cellinfo['cid']
+        req['cell_towers'] = [ { 'cell_id':cid, 'location_area_code':lac} ]
+        if 'rss' in cellinfo_keys: req['cell_towers'][0]['signal_strength'] = cellinfo['rss']
+    req['mobile_country_code'] = cellinfo['mcc'] if ('mcc' in cellinfo_keys) else 460 
+    req['mobile_network_code'] = cellinfo['mnc'] if ('mnc' in cellinfo_keys) else 0
+    if len(macs): req['wifi_towers'] = [{'mac_address':m, 'signal_strength':rsss[i]} for i,m in enumerate(macs)]
+    req['version'] = '1.1.0'
+    if atoken: req['access_token'] = atoken
+    req_json = json.dumps(req)
+    return req_json
+
+
+def connect_retry(f):
+    def wrapper(*arg, **ka):
+        delay = 1
+        while True:
+            try:
+                result = f(*arg, **ka)
+                break
+            except (sckt.error, ul.URLError), e:
+                if hasattr(e, 'code'):
+                    print(colors['red'] % ('HTTP Error: (%s): %s' % (e.code, e.msg)))
+                elif hasattr(e, 'reason'):
+                    print(colors['red'] % ('URL Error: %s!' % e.reason))
+                else: print e
+            except Exception, e:
+                print e
+            #if isinstance(retry, int):
+            #    if retry <= 0: break
+            #    else: retry -= 1
+            delay += 0.5; time.sleep(delay)
+            print colors['blue'] % '... Retrying ...'
+        return result
+    return wrapper
+
+@connect_retry
+def googleLocation(macs=None, rsss=None, cellinfo=None):
+    req_content = genLocReq(macs=macs, rsss=rsss, cellinfo=cellinfo)
+    req_url = "http://www.google.com/loc/json"
+    sckt.setdefaulttimeout(3)#; setProxy()
+    resp = ul.urlopen(req_url, req_content)
+    ret_content = dict( eval(resp.read()) )
+    if not 'location' in ret_content: return []
+    gloc = ret_content['location'] 
+    if gloc['accuracy'] > 5000: return []
+    gh = gloc['altitude'] if ('altitude' in gloc) else 0
+    return [ gloc['latitude'], gloc['longitude'], gh, gloc['accuracy'] ]
+
+
+def setProxy():
+    proxyserver = "http://proxy.cmcc:8080"
+    proxy = {'http': proxyserver}
+    #sckt.setdefaulttimeout(50)
+    opener = ul.build_opener( ul.ProxyHandler(proxy) )
+    ul.install_opener( opener )
+
+
+@connect_retry
+def googleGeocoding(latlon=(0,0), format='json', sensor='false'):
+    """return reverse geocoding result of google api."""
+    googleurl = 'http://maps.google.com/maps/api/geocode'
+    uri = '%s/%s?latlng=%s,%s&sensor=%s' % (googleurl,format,latlon[0],latlon[1],sensor)
+    resp = ul.urlopen(uri)
+    data = json.load(resp)
+    return data
+
+def collectCellArea():
+    query_cnt = 0 # Google Map API regular user limit: no more than 2500 queries/day
+    cell_area = {}
+    homedir = os.environ['HOME']; csvfile = '%s/wpp/wpp/util/cells_latlon.csv' % homedir
+    conn = db.connect('%s/wpp/wpp/util/cell_area.db'% homedir); cur = conn.cursor()
+    cells_loc = open(csvfile,'r').readlines()
+    for cell in cells_loc:
+        lac,cid,lat,lon = [ x.strip('\"') for x in cell.strip().split(',') ]
+        print '%s, %s, %s, %s' % (lac, cid, lat, lon)
+        sql = 'SELECT areacode from cell_area WHERE lac=%s AND cellid=%s'%(lac,cid)
+        cur.execute(sql)
+        areacodes = cur.fetchall()
+        if not areacodes:
+            latlon = (lat, lon)
+            geodata = googleGeocoding(latlon)
+            if geodata['status'] == u'OK':
+                query_cnt += 1; print 'query count: %s' % query_cnt
+                area_names = [ x['address_components'][::-1][1:] 
+                        for x in geodata['results'] if x['types'][0]=='sublocality' ]
+                if area_names: area_names = area_names[0]
+                else:
+                    area_names = [ x['address_components'][::-1][:-1] 
+                            for x in geodata['results'] if x['types'][0]=='street_address' ]
+                    if area_names: area_names = area_names[0]
+                    else: continue
+                area_name = [ x['long_name'] for x in area_names ]
+                area_district = area_name[-1]
+                if not area_district in area_codes: 
+                    print 'district: %s not in area_codes!' % area_district
+                    pp.pprint(geodata['results']); sys.exit(0)
+                area_code = area_codes[area_district]
+                if area_code in [x[0] for x in areacodes]: continue
+                area_name = '+'.join(area_name)
+                rec = '"%s","%s","%s","%s"' % (cid, area_code, area_name, lac)
+                cur.execute('INSERT INTO cell_area VALUES (%s)' % rec)
+                conn.commit()
+                print colors['blue'] % ('insert %s' % rec)
+            else: sys.exit(colors['red'] % ('ERROR: %s !!!' % geodata['status']))
+        else: continue
+        cur.execute('select count(*) from cell_area')
+        print 'Count: %s\n%s' % (cur.fetchone()[0], '-'*40)
+    conn.close()
+
+
+area_codes = {
+      "Dongcheng": "110101",
+        "Xicheng": "110102",
+       "Chongwen": "110103",
+         "Xuanwu": "110104",
+       "Chaoyang": "110105",
+        "Fengtai": "110106",
+    "Shijingshan": "110107",
+        "Haidian": "110108",
+      "Mentougou": "110109",
+       "Fangshan": "110111",
+       "Tongzhou": "110112",
+         "Shunyi": "110113",
+      "Changping": "110114",
+         "Daxing": "110115",
+        "Huairou": "110116",
+         "Pinggu": "110117",
+          "Miyun": "110228",
+        "Yanqing": "110229",
+          "Mawei": "350105",
+        "Changle": "350182",
+          "Gulou": "350102",
+         "Jin'an": "350111",
+       "Taijiang": "350103",
+       "Cangshan": "350104",
+        "Kunshan": "320583",
+        "Beitang": "320204",
+       "Chong'an": "320202",
+       "Nanchang": "320203",
+          "Anxin": "130632",
+     "Gaobeidian": "130684",
+       "Zhuozhou": "130681",
+       "Dingxing": "130626",
+      "Rongcheng": "130629",
+         "Xushui": "130625",
+         "Jixian": "120225",
+       "Dongling": "210112",
+          "Daoli": "230102",
+       "Jinjiang": "350582",
+       "Changtai": "350625",
+       "Kongtong": "620802",
+           "Jimo": "370282",
+         "Shibei": "370203",
+         "Sifang": "370205",
+          "Yanta": "610113",
+        "Chiping": "371523",
+       "Qingyang": "510105",
+      "Pingjiang": "430626",
+         "Jiyuan": "419001",
+         "Tianhe": "440106",
+       "Bao'anqu": "440306",
+         "Xishan": "320205",
+        "Fucheng": "510703",
+        "Youxian": "510704",
+         "Hal'an": "320621",
+          "Laixi": "370285",
+         "Nanhai": "440605",
+           "Anji": "330523",
+       "Jiangbei": "330205",
+         "Doumen": "440403",
+      "Jiangning": "320115",
+       "Jiangyan": "321284",
+      "Shouguang": "370783",
+         "Tinghu": "320902",
+         "Lianhu": "610104",
+          "Qixia": "320113",
+      "Tai Ha St": "810000",
+          "Binhu": "320211", }
+
+if __name__ == "__main__":
+    try:
+        import psyco
+        psyco.bind(genLocReq)
+        #psyco.bind(googleLocation)
+        psyco.bind(setProxy)
+        #psyco.bind(googleGeocoding)
+    except ImportError:
+        pass
+
+    #setProxy()
+    
+    collectCellArea()
+
+    sys.exit(0)
+    #wlans = ['00:21:91:1D:C0:D4|00:27:19:88:97:10|00:19:E0:CC:9C:F8|00:23:CD:54:DC:E0','-83|-86|-85|-88']
+    #macs = wlans[0].split('|')
+    #rsss = wlans[1].split('|')
+    #wlans = []
+    #for i,mac in enumerate(macs):
+    #    wlan = {}
+    #    wlan['mac_address'] = mac
+    #    wlan['signal_strength'] = rsss[i]
+    #    wlans.append(wlan)
+    #print wlans
+
+    # csvfile format: x,y,lac,cid
+    csvfile = sys.argv[1] 
+    csvin = csv.reader( open(csvfile,'r') )
+    laccids = np.array([ line for line in csvin ])[:,2:].astype(int)
+    print laccids
+
+    atoken = None; celldb = []
+    for i,laccid in enumerate(laccids):
+        print '-'*50
+        cell = {}
+        cell['location_area_code'] = laccid[0]
+        cell['cell_id'] = laccid[1]
+        cells = [ cell ]
+        req_content = genLocReq(cells=cells, atoken=atoken)
+        print '%d: %s' % (i+1, json.loads(req_content)['cell_towers'])
+        ret_content = googleLocation(req_content)
+        if not len(ret_content): 
+            print colors['red'] % 'Google location failed!'
+            continue
+        if ret_content['location']['accuracy'] >= 1000: 
+            print colors['red'] % 'Accuracy too bad!'
+            continue
+        cdb = [ str(laccid[0]), str(laccid[1]),
+                str(ret_content['location']['latitude']), 
+                str(ret_content['location']['longitude']),
+                str(ret_content['location']['accuracy']) ]
+        if (not atoken) and ('access_token' in ret_content):
+            atoken = ret_content['access_token']
+        celldb.append(cdb)
+        print '%d: %s' % (len(celldb), cdb)
+
+    timestamp = time.strftime('%Y%m%d-%H%M%S')
+    datafile = 'celldb_%s.csv' % timestamp
+    celldb = np.array(celldb)
+    np.savetxt(datafile, celldb, fmt='%s',delimiter=',')
+    print '\nDumping all celldb to: %s ... Done' % datafile
