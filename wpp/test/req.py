0a3d47339968a0130de02fc08a696610a59a33b2
wpp <192.168.109.56@localhost>
2012-01-16 17:16:02 +0800

    Add fpp jiangsu config for elastic google location data crawling; Mod Google fail limit config.

--- wpp/test/req.py
+++ wpp/test/req.py
@@ -20,7 +20,7 @@ from wpp.util.net import getIP
 #req_wpp = """<?xml version='1.0' encoding='UTF-8' standalone='no' ?><!DOCTYPE PosReq SYSTEM "PosReq.dtd"><PosReq><SPID val="1000" /><ServID val="1000101" /><Time val="20110725-141421" /><UserInfo imei="012682003400042" imsi="460007316414690" UserAgent="SEMC/LT15i_1247-1052" /><CellInfo mcc="460" mnc="00" lac="1015" cid="28837" rss="-63" /><WLANIdentifier val="00:21:27:61:16:36|00:23:cd:3f:2d:b6" /><WLANMatcher val="-88|-98" /><PosLevel val="Hybrid" /></PosReq>"""
 #req_wpp = """<?xml version='1.0' encoding='UTF-8' standalone='no' ?><!DOCTYPE PosReq SYSTEM "PosReq.dtd"><PosReq><SPID val="1000" /><ServID val="1000112" /><Time val="20111113-083933" /><UserInfo imei="352284040778213" imsi="460001343119748" UserAgent="Samsung/OMAP_SS" /><CellInfo mcc="460" mnc="00" lac="4141" cid="30472" rss="-49" /><WLANIdentifier val="00:17:7b:0f:7a:b0|fc:75:16:1f:19:7a|00:17:7b:0f:0f:58|54:e6:fc:41:72:26|40:16:9f:d2:db:0e|00:14:5a:01:55:8f|74:ea:3a:1a:62:c8|00:14:5a:01:53:ae|38:e7:d8:b2:06:c1|f0:7d:68:4d:6f:aa" /><WLANMatcher val="-70|-78|-80|-87|-88|-88|-89|-90|-91|-93" /><PosLevel val="Point" /></PosReq>"""
 #req_wpp = """<?xml version='1.0' encoding='UTF-8' standalone='no' ?><!DOCTYPE PosReq SYSTEM "PosReq.dtd"><PosReq><SPID val="1000" /><ServID val="1000112" /><Time val="20111208-092935" /><UserInfo imei="352284040778213" imsi="460001343119748" UserAgent="Samsung/OMAP_SS" /><CellInfo mcc="460" mnc="00" lac="4141" cid="26390" rss="-49" /><WLANIdentifier val="3c:e5:a6:93:88:f0|00:23:89:f3:a0:10|c4:ca:d9:01:77:c0|c4:ca:d9:01:82:f0|3c:e5:a6:93:d8:30|b4:b3:62:49:bf:ab|3c:e5:a6:93:b7:50|00:23:89:f3:2f:50|c4:ca:d9:01:6d:c0|c4:ca:d9:01:79:70" /><WLANMatcher val="-39|-52|-54|-65|-70|-75|-76|-78|-79|-82" /><PosLevel val="Point" /></PosReq>"""
-req_wpp = """<?xml version='1.0' encoding='UTF-8' standalone='no' ?><!DOCTYPE PosReq SYSTEM "PosReq.dtd"><PosReq><SPID val="1000" /><ServID val="1000112" /><Time val="20111209-235501" /><UserInfo imei="355066049059381" imsi="460015180303733" UserAgent="htc/htc_pyramid" /><CellInfo mcc="460" mnc="01" lac="53505" cid="100732795" rss="0" /><WLANIdentifier val="00:25:86:4b:41:64|00:25:12:81:4c:ff|00:1a:70:e5:2d:19|94:0c:6d:d7:96:68|b0:48:7a:76:fd:b0" /><WLANMatcher val="-70|-86|-89|-85|-91" /><PosLevel val="Point" /></PosReq>"""
+req_wpp = """<?xml version='1.0' encoding='UTF-8' standalone='no' ?><!DOCTYPE PosReq SYSTEM "PosReq.dtd"><PosReq><SPID val="1000" /><ServID val="1000112" /><Time val="20120116-144044" /><UserInfo imei="358000048640591" imsi="460001994912200" UserAgent="twm_tw/htc_vivo" /><CellInfo mcc="460" mnc="00" lac="37357" cid="55062" rss="-71" /><PosLevel val="Point" /></PosReq>"""
 # cell invalid for fail_count test.
 #req_wpp = """<?xml version='1.0' encoding='UTF-8' standalone='no' ?><!DOCTYPE PosReq SYSTEM "PosReq.dtd"><PosReq><SPID val="1000" /><ServID val="1000101" /><Time val="20110725-141421" /><UserInfo imei="012682003400042" imsi="460007316414690" UserAgent="SEMC/LT15i_1247-1052" /><CellInfo mcc="460" mnc="00" lac="1011111111" cid="28837" rss="-63" /><PosLevel val="Point" /></PosReq>"""
 # WPP OK, PosLevel Null.
@@ -33,9 +33,11 @@ req_wpp = """<?xml version='1.0' encoding='UTF-8' standalone='no' ?><!DOCTYPE Po
 # Cell + WLAN, ONLY Cell OK.
 #req_fpp = """<?xml version="1.0"?><!DOCTYPE PosReq_FC SYSTEM "PosReq_FC.dtd"><PosReq_FC><SPID val="123" /><ServID val="456" /><Time val="20110130-114424" /><UserInfo imei="355302043446609" imsi="460001933113172" UserAgent="htc_asia_wwe/htc_bravo" /><CellInfo mcc="460" mnc="00" lac="4577" cid="3088" rss="-63" /><WLANIdentifier val="00:15:70:9f:62:6a" /><WLANMatcher val="-40" /></PosReq_FC>"""
 # Illegal msg content, e.g. imei.
-req_fpp = """<?xml version="1.0"?><!DOCTYPE PosReq_FC SYSTEM "PosReq_FC.dtd"><PosReq_FC><SPID val="123" /><ServID val="456" /><Time val="20110130-114424" /><UserInfo imei="AB5302043446609" imsi="460001933113172" UserAgent="htc_asia_wwe/htc_bravo" /><CellInfo mcc="460" mnc="00" lac="4577" cid="3088" rss="-63" /><WLANIdentifier val="00:15:70:9f:62:6a" /><WLANMatcher val="-40" /></PosReq_FC>"""
+#req_fpp = """<?xml version="1.0"?><!DOCTYPE PosReq_FC SYSTEM "PosReq_FC.dtd"><PosReq_FC><SPID val="123" /><ServID val="456" /><Time val="20110130-114424" /><UserInfo imei="AB5302043446609" imsi="460001933113172" UserAgent="htc_asia_wwe/htc_bravo" /><CellInfo mcc="460" mnc="00" lac="4577" cid="3088" rss="-63" /><WLANIdentifier val="00:15:70:9f:62:6a" /><WLANMatcher val="-40" /></PosReq_FC>"""
 # Cell + WLAN, Both OK, WLAN Better.
 #req_fpp = """<?xml version="1.0"?><!DOCTYPE PosReq_FC SYSTEM "PosReq_FC.dtd"><PosReq_FC><SPID val="123" /><ServID val="456" /><Time val="20110130-114424" /><UserInfo imei="355302043446609" imsi="460001933113172" UserAgent="htc_asia_wwe/htc_bravo" /><CellInfo mcc="460" mnc="00" lac="4577" cid="3088" rss="-63" /><WLANIdentifier val="00:15:70:9f:62:64|00:25:86:4a:b9:6e|00:15:70:9f:72:0c|00:15:70:d0:52:60|00:23:cd:68:f6:d6|00:1b:11:a4:2d:c6|00:15:70:a6:a3:30|00:15:70:9f:62:66|00:15:70:9f:72:0e|00:15:70:d0:52:62|00:25:86:51:bd:54|00:15:70:a6:a3:32|00:1c:10:aa:c0:a8|00:11:b5:fd:76:d4" /><WLANMatcher val="-40|-61|-70|-72|-75|-85|-85|-40|-69|-72|-82|-84|-88|-59" /></PosReq_FC>"""
+# FPP Jiangsu.
+req_fpp = """<?xml version="1.0"?><!DOCTYPE PosReq_FC SYSTEM "PosReq_FC.dtd"><PosReq_FC><SPID val="1000" /><ServID val="1000112" /><Time val="20120116-144044" /><UserInfo imei="358000048640591" imsi="460001994912200" UserAgent="twm_tw/htc_vivo" /><CellInfo mcc="460" mnc="00" lac="37357" cid="55062" rss="-71" /><PosLevel val="Point" /></PosReq_FC>"""
 # Cell + WLAN, ONLY WLAN OK.
 #req_fpp = """<?xml version="1.0"?><!DOCTYPE PosReq_FC SYSTEM "PosReq_FC.dtd"><PosReq_FC><SPID val="123" /><ServID val="456" /><Time val="20110130-114424" /><UserInfo imei="355302043446609" imsi="460001933113172" UserAgent="htc_asia_wwe/htc_bravo" /><CellInfo mcc="460" mnc="00" lac="4577777" cid="3088" rss="-63" /><WLANIdentifier val="00:15:70:9f:62:64|00:25:86:4a:b9:6e|00:15:70:9f:72:0c|00:15:70:d0:52:60|00:23:cd:68:f6:d6|00:1b:11:a4:2d:c6|00:15:70:a6:a3:30|00:15:70:9f:62:66|00:15:70:9f:72:0e|00:15:70:d0:52:62|00:25:86:51:bd:54|00:15:70:a6:a3:32|00:1c:10:aa:c0:a8|00:11:b5:fd:76:d4" /><WLANMatcher val="-40|-61|-70|-72|-75|-85|-85|-40|-69|-72|-82|-84|-88|-59" /></PosReq_FC>"""
 #--------------------------fpp uprecs reqs--------------------------
@@ -55,6 +57,7 @@ ip_wpp_cmri = '192.168.109.56'
 ip_fpp_moto = '192.168.109.56' 
 ip_wpp_local = 'localhost'
 ip_fpp_neu = '192.168.109.58'
+ip_fpp_neu_js = '112.25.187.195'
 
 port_wpp_cmri = '8080'
 port_wpp_vance = '8081'
@@ -101,6 +104,11 @@ targets = {
                 'port':port_fpp_neu, 
              'urlpath':urlpath_fpp_neu,
             'req_data':req_fpp},
+       'fpp_neu_js':{
+                  'ip':ip_fpp_neu_js, 
+                'port':port_fpp_neu, 
+             'urlpath':urlpath_fpp_neu,
+            'req_data':req_fpp},
        'fpp_neu':{
                   'ip':ip_fpp_neu, 
                 'port':port_fpp_neu, 
@@ -120,13 +128,13 @@ if __name__ == "__main__":
     else: 
         ip_src = ips_src['eth0']
 
-    target = targets['wpp_local']
+    target = targets['fpp_neu_js']
     #target['urlpath'] = target['urlpath'].lower() # compatible with local fpp urlpath.
     # urlpath & port for cmcc mobile naviwap test.
-    #target['ip'] = '211.137.35.180'
-    #target['port'] = '8081'
-    #target['urlpath'] = 'naviwap/MELocationAction.do'
-    #target['req_data'] = req_uprecs
+    #target['ip'] = '112.25.187.195'
+    #target['port'] = '18080'
+    #target['urlpath'] = 'FPP_AD/adapter'
+    #target['req_data'] = req_wpp
     url = 'http://%s:%s/%s' % (target['ip'], target['port'], target['urlpath'])
     print '\nRequesting %s from %s\n' % (url, ip_src)
     req_data = target['req_data']
